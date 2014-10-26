a0099bcfa4786f7f4b5c110ecb52ccc39433c29d
Felipe Dau <felipe-dealcantaradau@uiowa.edu>
2014-10-26 18:11:43 -0300

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soundcloudradio::Application.config.secret_token = 'eb10cdbcd6827ed636bc869cf8f7975cc59e67f2527418ab52a1258beee8021ad9402e2cdb7b740dd33268f5cc0c02026850dfad08e1d27a6b2e5c6aaf24cf45'

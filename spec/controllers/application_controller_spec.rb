df958998d5a3d4637cd9ff8447500a60bb1e48ef
cbonezzi <cesarenrique-bonezzirincon@uiowa.edu>
2014-11-14 15:26:27 -0600

    rspec skeleton

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe ApplicationController do
+  describe 'set current user' do
+    it 'should check for session token'
+    it 'should check current user'
+    it 'should check username'
+  end
+end
